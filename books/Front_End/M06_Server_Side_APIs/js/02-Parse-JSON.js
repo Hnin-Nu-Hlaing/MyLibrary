4b4c68b6fc9a026fe2a826e6cb5ebfd413a15f9a
Hnin-Nu-Hlaing <sakurayk2002@gmail.com>
2024-09-29 13:19:09 +0630

    activies

--- books/Front_End/M06_Server_Side_APIs/js/02-Parse-JSON.js
+++ books/Front_End/M06_Server_Side_APIs/js/02-Parse-JSON.js
@@ -1,4 +1,4 @@
-var requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
+var requestUrl = 'https://api.github.com/users/Hnin-Nu-Hlaing/repos?per_page=5';
 
 fetch(requestUrl)
   .then(function (response) {
@@ -8,6 +8,6 @@ fetch(requestUrl)
     console.log('Github Repo Issues \n----------');
     for (var i = 0; i < data.length; i++) {
       console.log(data[i].url);
-      console.log(data[i].user.login);
+      console.log(data[i].owner.login);
     }
   });
